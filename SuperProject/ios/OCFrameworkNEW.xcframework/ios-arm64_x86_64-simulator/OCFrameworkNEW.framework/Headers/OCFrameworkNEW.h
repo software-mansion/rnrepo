103f92515204d3428296e04933826b2f21611d7a
Michał Czernek <czernekmichal@gmail.com>
2025-06-05 11:17:10 +0200

    Framework

---
+++ SuperProject/ios/OCFrameworkNEW.xcframework/ios-arm64_x86_64-simulator/OCFrameworkNEW.framework/Headers/OCFrameworkNEW.h
@@ -0,0 +1,17 @@
+//
+//  OCFrameworkNEW.h
+//  OCFrameworkNEW
+//
+//  Created by Krzysztof Sliwinski on 05/05/2025.
+//
+
+#import <Foundation/Foundation.h>
+#import "MyFrameworkTestViewManager.h"
+
+//! Project version number for OCFrameworkNEW.
+FOUNDATION_EXPORT double OCFrameworkNEWVersionNumber;
+
+//! Project version string for OCFrameworkNEW.
+FOUNDATION_EXPORT const unsigned char OCFrameworkNEWVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OCFrameworkNEW/PublicHeader.h>
