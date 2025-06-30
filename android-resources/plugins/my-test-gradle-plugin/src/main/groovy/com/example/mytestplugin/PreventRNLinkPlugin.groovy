86e04d6ae62e84ea2908176d802507e532e7c17c
Krzysztof Śliwiński <krzysztof.sliwinski@swmansion.com>
2025-06-30 15:07:02 +0200

    chore: move autolinking maven dependencies from react extension to custom gradle plugin

--- android-resources/plugins/my-test-gradle-plugin/src/main/groovy/com/example/mytestplugin/PreventRNLinkPlugin.groovy
+++ android-resources/plugins/my-test-gradle-plugin/src/main/groovy/com/example/mytestplugin/PreventRNLinkPlugin.groovy
@@ -1,37 +1,59 @@
 package com.example.mytestplugin
 
+import groovy.json.JsonSlurper
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import groovy.json.JsonSlurper
-import groovy.json.JsonBuilder
-import java.nio.file.Files
-import java.nio.file.Paths
+import java.io.File
 
 class PreventRNLinkPlugin implements Plugin<Project> {
     void apply(Project project) {
-        project.tasks.register("removeReanimatedDependency") {
-            doLast {
-                def path = Paths.get(project.rootDir.path, "build/generated/autolinking/autolinking.json")
-                def file = path.toFile()
- 
-                if (file.exists()) {
-                    def jsonSlurper = new JsonSlurper()
-                    def originalJson = jsonSlurper.parse(file) 
-                    def json = originalJson.toMutableMap()
-                    def originalDependencies = json["dependencies"]
-                    originalDependencies?.let {
-                        def dependencies = it.toMutableMap()
-                        dependencies.remove("react-native-reanimated")
-                        json["dependencies"] = dependencies
-                        println("MODIFIED: " + json["dependencies"])
-                        def modifiedJsonString = new JsonBuilder(json).toPrettyString()
-                        Files.write(path, modifiedJsonString.bytes)
-                        println("Modified JSON saved.")
-                    }
-                } else {
-                    println("The specified JSON file does not exist: ${file.path}")
+        project.plugins.withId('com.android.application') {
+            project.afterEvaluate {
+                handleAutoLinking(project)
+            }
+        }
+    }
+
+    void handleAutoLinking(Project project) {
+        File inputFile = project.rootProject.layout.buildDirectory
+                                .file("generated/autolinking/autolinking.json")
+                                .get().asFile
+        
+        Map dependencyInfo = getGradleDependenciesToApply(inputFile)
+        List<Map.Entry<String, String>> dependenciesToApply = dependencyInfo.dependenciesToApply
+        List<String> dependenciesToRemove = dependencyInfo.dependenciesToRemove
+
+        // Exclude dependencies
+        project.configurations.all { config ->
+            dependenciesToRemove.each { depName ->
+                config.exclude module: depName.replaceAll(":", "")
+            }
+        }
+    
+        
+        // Add only maven dependencies
+        dependenciesToApply.each { Map.Entry<String, String> dependencyEntry ->
+            String configuration = dependencyEntry.key
+            String path = dependencyEntry.value
+            project.dependencies.add(configuration, dependencyEntry.value)
+        }
+    }
+
+    Map getGradleDependenciesToApply(File inputFile) {
+        def model = new JsonSlurper().parse(inputFile)
+        def result = []
+        def dependenciesToRemove = []
+        
+        model.dependencies.each { depName, depDetails ->
+            if (depDetails.platforms?.android) {
+                String dependencyConfiguration = depDetails.platforms.android.dependencyConfiguration ?: 'implementation'
+                String mavenDependency = depDetails.platforms.android.mavenDependency
+                if (mavenDependency) {
+                    result << new AbstractMap.SimpleEntry(dependencyConfiguration, mavenDependency)
+                    dependenciesToRemove << depName
                 }
             }
         }
+        return [dependenciesToApply: result, dependenciesToRemove: dependenciesToRemove]
     }
 }
