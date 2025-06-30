ce0e6f580f850c23c1bfd5ec942ce7230096d82d
Bartosz Stefańczyk <bartosz.stefanczyk@swmansion.com>
2025-06-30 07:55:00 +0200

    feat: introduce build script for automated-ish XCFramework building

--- OCFrameworkNEW/OCFrameworkNEW/MyFrameworkTestViewManager.m
+++
@@ -1,9 +0,0 @@
-#import "MyFrameworkTestViewManager.h"
-#import <React/RCTBridge.h>
-#import <UIKit/UIKit.h>
-
-@implementation MyFrameworkTestViewManager
-
- RCT_EXPORT_MODULE(MyFrameworkTestView)
-
-@end
--- SuperProject/ios/OCFrameworkNEW.xcframework/ios-arm64/OCFrameworkNEW.framework/Headers/MyFrameworkTestViewManager.h
+++
@@ -1,8 +0,0 @@
-#import <React/RCTViewManager.h>
-
-
-@interface MyFrameworkTestViewManager : RCTViewManager
-
-// You would declare native methods you'd want to access from the view here
-
-@end
