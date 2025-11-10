1d10db2b57fc3ef62466298820c3fdb87bb0454d
Krzysztof Magiera <krzys@swmansion.com>
2025-11-10 15:19:36 +0100

    Fix config script

--- packages/config/src/index.ts
+++ packages/config/src/index.ts
@@ -1,13 +1,10 @@
 // Export libraries configuration (from root)
-import libraries from '../../libraries.json';
+import libraries from '../../../libraries.json';
 export { libraries };
-export default libraries;
 
 // Export React Native versions (from root)
-import reactNativeVersions from '../../react-native-versions.json';
+import reactNativeVersions from '../../../react-native-versions.json';
 export { reactNativeVersions };
-export default reactNativeVersions;
 
 // Re-export types
 export type { LibraryConfig } from './types';
-
