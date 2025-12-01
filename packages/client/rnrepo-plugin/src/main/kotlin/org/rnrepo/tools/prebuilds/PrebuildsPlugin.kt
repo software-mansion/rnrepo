43ec2baff489e40ed75a1740b7599beb78ebfd32
Radosław Rolka <radoslaw.rolka@gmail.com>
2025-12-01 10:30:07 +0100

    fix log (#86)

--- packages/client/rnrepo-plugin/src/main/kotlin/org/rnrepo/tools/prebuilds/PrebuildsPlugin.kt
+++ packages/client/rnrepo-plugin/src/main/kotlin/org/rnrepo/tools/prebuilds/PrebuildsPlugin.kt
@@ -625,8 +625,8 @@ class PrebuildsPlugin : Plugin<Project> {
             logger.lifecycle(
                 "Packages not substituted – will fallback to building from sources: ${
                     unavailablePackages.joinToString(
-                        separator = "\n  - ❓ ",
-                    ) { "${it.name}@${it.version}${it.classifier} for React Native ${extension.reactNativeVersion}" }
+                        "",
+                    ) { "\n  - ❓ ${it.name}@${it.version}${it.classifier} for React Native ${extension.reactNativeVersion}" }
                 }",
             )
         }
