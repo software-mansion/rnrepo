fb02bb702d652f99b98b4a587dcdb8352eac9dbf
Radosław Rolka <radoslaw.rolka@gmail.com>
2025-12-03 16:11:56 +0100

    [RNGH] Decouple gesture-handler and svg with patch
    When building react-native-gesture-handler, we no longer have to create 2 artifacts
    for different variants (with and without svg support) because now react-native-svg
    does not modify the sourceSets of RNGH.

--- postInstallScripts/react-native-gesture-handler/react-native-gesture-handler.ts
+++ postInstallScripts/react-native-gesture-handler/react-native-gesture-handler.ts
@@ -8,6 +8,13 @@ export default async function postInstallSetup(): Promise<void> {
     console.log('✓ Installed react-native-reanimated@4.0.0');
     await $`bun install react-native-worklets@0.4.0 --save-exact`.quiet();
     console.log('✓ Installed react-native-worklets@0.4.0');
+    // Patch react-native-gesture-handler to avoid issues with react-native-svg
+    await $`bun install react-native-svg`.quiet();
+    console.log('✓ Installed react-native-svg');
+    const patchPath = __dirname + '/react-native-gesture-handler.patch';
+    const packagePath = './node_modules/react-native-gesture-handler';
+    await $`/usr/bin/patch -p1 -i ${patchPath}`.cwd(packagePath).quiet();
+    console.log('✓ Patched react-native-gesture-handler for SVG compatibility');
     
     console.log(`✓ Post-install setup for react-native-gesture-handler completed.`);
 };
