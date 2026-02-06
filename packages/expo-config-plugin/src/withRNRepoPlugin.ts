654cf2e7206f1ebfe4729fc3920fa7fdfb2d095c
Radosław Rolka <radoslaw.rolka@gmail.com>
2026-02-06 11:43:23 +0100

    revert iOS changes in expo plugin until released

--- packages/expo-config-plugin/src/withRNRepoPlugin.ts
+++ packages/expo-config-plugin/src/withRNRepoPlugin.ts
@@ -1,7 +1,5 @@
-import { withProjectBuildGradle, withAppBuildGradle, withDangerousMod } from '@expo/config-plugins'; 
+import { withProjectBuildGradle, withAppBuildGradle } from '@expo/config-plugins'; 
 import type { ExpoConfig } from '@expo/config-types';
-import * as fs from 'fs';
-import * as path from 'path';
 
 // Android
 const classpathRegex = /(classpath.*)/;
@@ -23,14 +21,6 @@ allprojects {
         maven { url "https://packages.rnrepo.org/releases" }
     }
 }`;
-// iOS
-const podfileRequire = `require Pod::Executable.execute_command('node', ['-p',
-  'require.resolve(
-  "@rnrepo/build-tools/cocoapods-plugin/lib/plugin.rb",
-  {paths: [process.argv[1]]},
-)', __dir__]).strip`;
-const postInstallRegex = /(post_install do \|installer\|)/;
-const postInstallRNRepo = `rnrepo_post_install(installer)`;
 
 function withAllProjectsMavenRepository(config: ExpoConfig) {
   return withProjectBuildGradle(config, (config) => {
@@ -68,41 +58,9 @@ function withRnrepoPluginApplication(config: ExpoConfig) {
   });
 }
 
-function withRNRepoPodfile(config: ExpoConfig) {
-  return withDangerousMod(config, [
-    'ios',
-    async (config) => {
-      const podfilePath = path.join(config.modRequest.platformProjectRoot, 'Podfile');
-      
-      if (fs.existsSync(podfilePath)) {
-        let podfileContent = fs.readFileSync(podfilePath, 'utf8');
-        const originalPodfileContent = podfileContent;
-        
-        if (!podfileContent.includes('@rnrepo/build-tools/cocoapods-plugin/lib/plugin.rb')) {
-          podfileContent = `${podfileRequire}\n\n${podfileContent}`;
-        }
-        
-        if (!podfileContent.includes('rnrepo_post_install')) {
-          podfileContent = podfileContent.replace(
-            postInstallRegex,
-            `$1\n  ${postInstallRNRepo}`
-          );
-        }
-        
-        if (podfileContent !== originalPodfileContent) {
-          fs.writeFileSync(podfilePath, podfileContent);
-        }
-      }
-      
-      return config;
-    },
-  ]);
-}
-
 export default function withRNRepoPlugin(config: ExpoConfig): ExpoConfig {
   config = withClasspathDependency(config);
   config = withRnrepoPluginApplication(config);
   config = withAllProjectsMavenRepository(config);
-  config = withRNRepoPodfile(config);
   return config;
 }
