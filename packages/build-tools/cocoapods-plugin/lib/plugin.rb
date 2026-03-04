061f1e5d53ca1ddee988faf65730dc68345661b5
Radosław Rolka <radoslaw.rolka@gmail.com>
2026-03-04 14:49:51 +0100

    works

--- packages/build-tools/cocoapods-plugin/lib/plugin.rb
+++ packages/build-tools/cocoapods-plugin/lib/plugin.rb
@@ -415,5 +415,39 @@ def rnrepo_post_install(installer_context)
 
   CocoapodsRnrepo::Logger.log "✓ Build phase scripts configured"
   CocoapodsRnrepo::Logger.log ""
+
+  installer_context.pods_project.targets.each do |target|
+    if target.name == 'ExpoModulesCore'
+      target.build_configurations.each do |config|
+        
+        # --- HEADER SEARCH PATHS ---
+        # Add new paths while preserving existing ones
+        header_search_paths = config.build_settings['HEADER_SEARCH_PATHS'] || '$(inherited)'
+        header_search_paths += ' "$(SRCROOT)/../../node_modules/react-native-worklets/Common/cpp/**"'
+        header_search_paths += ' "$(SRCROOT)/../node_modules/react-native-worklets/Common/cpp/**"'
+        config.build_settings['HEADER_SEARCH_PATHS'] = header_search_paths
+
+        # --- USER HEADER SEARCH PATHS ---
+        user_search_paths = config.build_settings['USER_HEADER_SEARCH_PATHS'] || ['$(inherited)']
+        user_search_paths << '"$(SRCROOT)/../../node_modules/react-native-worklets/Common/cpp"'
+        config.build_settings['USER_HEADER_SEARCH_PATHS'] = user_search_paths
+
+        # --- CUSTOM COMPILER FLAGS (C/C++) ---
+        # Note: Consider making this path relative to $(SRCROOT) if possible
+        module_map_flag = '-fmodule-map-file="$(SRCROOT)/../../node_modules/react-native-worklets/.rnrepo-cache/Debug/RNWorklets.xcframework/ios-arm64_x86_64-simulator/RNWorklets.framework/Modules/module.modulemap"'
+        
+        other_cflags = config.build_settings['OTHER_CFLAGS'] || '$(inherited)'
+        other_cflags += ' ' + module_map_flag
+        config.build_settings['OTHER_CFLAGS'] = other_cflags
+
+        # --- OTHER SWIFT FLAGS ---
+        other_swift_flags = config.build_settings['OTHER_SWIFT_FLAGS'] || '$(inherited)'
+        other_swift_flags +=  ' -Xcc'
+        other_swift_flags += ' ' + module_map_flag
+        config.build_settings['OTHER_SWIFT_FLAGS'] = other_swift_flags
+
+      end
+    end
+  end
 end
 
