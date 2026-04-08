de3026ae3c6a4e501e8555233f04dcbb8e97f2d5
Radosław Rolka <radoslaw.rolka@gmail.com>
2026-04-09 01:00:05 +0200

    fix failing ci during pod install on ubuntu runners
    Pod install is not used in building emc, so it can be handled in catch

--- preInstallScripts/expo-modules-core/expo-modules-core.ts
+++ preInstallScripts/expo-modules-core/expo-modules-core.ts
@@ -7,7 +7,24 @@ export default async function preInstallSetup(): Promise<void> {
     await $`npm install`.quiet();
     
     console.log(`Installing expo-modules with 'install-expo-modules@latest' command...`);
-    await $`npx install-expo-modules@latest --non-interactive`.quiet();
+    await $`rm -rf ios`.quiet(); // skip pod install from install-expo-modules
+    try {
+        await $`npx install-expo-modules@latest --non-interactive`.quiet();
+    } catch (error) {
+        let isExpectedFailure = false;
+
+        if (error instanceof $.ShellError) {
+            const output = (error.stderr.toString() + error.stdout.toString()).toLowerCase();
+            isExpectedFailure = output.includes('command `pod install` failed');
+        }
+
+        if (isExpectedFailure) {
+            console.warn('⚠️  iOS Pod install failed as expected, continuing...');
+        } else {
+            console.error('❌ Error during expo-modules installation:', error);
+            throw error;
+        }
+    }
     
     console.log(`✓ Pre-install setup for expo-modules-core completed.`);
 };
